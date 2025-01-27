b8bf47c45e2bf572d821953c3be9aecaa3c5bf6a
2DC <kkts9308@gmail.com>
2025-01-27 16:58:46 +0900

    💄 set simple style code

--- src/client/layout/default/header.tsx
+++ src/client/layout/default/header.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import styles from "./css/header.module.css";
 
 export function Header() {
   return (
-    <header>
-      <nav>
+    <header className={styles.header}>
+      <h1 className={styles["header-title"]}>모두의 채팅</h1>
+      <nav className={styles.nav}>
         <li>
           <Link to={"/all-chat"}>모두 채팅</Link>
         </li>
